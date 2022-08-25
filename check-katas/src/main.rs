6e05950c30e5ac2406585b56f5f80211df3328f7
lincot <lincot@disroot.org>
2022-08-25 13:21:15 +0300

    ditch box and print

--- check-katas/src/main.rs
+++ check-katas/src/main.rs
@@ -1,13 +1,21 @@
 use my_prelude::prelude::*;
-use std::{fs::File, io::Read};
+use std::{
+    fs::File,
+    io::{self, Read, Write},
+};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut stdout = io::stdout().lock();
+
     let mut kyu_path = *b"7kyu";
 
-    for k in 1..=8 {
-        kyu_path[0] = b'0' + k;
-        let rd = std::fs::read_dir(unsafe { std::str::from_utf8_unchecked(&kyu_path) }).unwrap();
-        println!("checking {} kyu", k);
+    for k in b'1'..=b'8' {
+        kyu_path[0] = k;
+        let rd = std::fs::read_dir(unsafe { std::str::from_utf8_unchecked(&kyu_path) })?;
+        stdout.write(b"checking ")?;
+        stdout.write(&[k])?;
+        stdout.write(b" kyu\n")?;
+        stdout.flush()?;
         for d in rd {
             let d = d?.path().into_os_string();
             let d = d.to_str().unwrap();
@@ -17,25 +25,46 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 path.push_str_unchecked("/src/lib.rs");
             }
 
-            let id = get_id(&path)?;
-            let kata = get_kata(id)?;
+            let mut f = File::open(&path)?;
+            let mut buf = [0;
+                "//! <https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/rust>".len()];
+            f.read(&mut buf)?;
+            if !(buf.starts_with(b"//! <https://www.codewars.com/kata/")
+                && buf.ends_with(b"/train/rust>"))
+            {
+                stdout.write(path.as_bytes())?;
+                stdout.write(b" has invalid url")?;
+                stdout.write(b"\n")?;
+                stdout.flush()?;
+            }
+            let id = &buf["//! <https://www.codewars.com/kata/".len()
+                .."//! <https://www.codewars.com/kata/53da3dbb4a5168369a0000fe".len()];
+            let id = id.try_into().unwrap();
 
-            let kyu = get_kyu(&kata);
+            let (kata, kata_len) = get_kata(id)?;
+            let kata = unsafe { std::str::from_utf8_unchecked(kata.get_unchecked(..kata_len)) };
+
+            let kyu = get_kyu(kata);
             if k != kyu {
-                eprintln!("{:?} has obsolete kyu: should be {}", d, kyu);
+                stdout.write(d.as_bytes())?;
+                stdout.write(b" has obsolete kyu: should be ")?;
+                stdout.write(&[kyu])?;
+                stdout.write(b"\n")?;
+                stdout.flush()?;
             }
 
-            let slug = get_slug(&kata);
+            let slug = get_slug(kata);
             if slug.as_bytes().contains(&b'\\') {
                 continue;
             }
 
             let directory_name = &d[kyu_path.len() + 1..];
             if directory_name != slug {
-                eprintln!(
-                    "{:?} has obsolete directory name: should be \"{}\"",
-                    d, slug
-                );
+                stdout.write(d.as_bytes())?;
+                stdout.write(b" has obsolete directory name: should be ")?;
+                stdout.write(slug.as_bytes())?;
+                stdout.write(b"\n")?;
+                stdout.flush()?;
             }
 
             unsafe {
@@ -45,19 +74,26 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 }
                 slice.copy_from_slice(b"/Cargo.toml");
             }
-            let crate_name = get_crate_name(&path)?;
+            let (buf, name_pos, name_end) = get_crate_name(&path)?;
+            let crate_name =
+                unsafe { std::str::from_utf8_unchecked(buf.get_unchecked(name_pos..name_end)) };
             if crate_name.as_bytes() != slug.as_bytes() {
                 if (b'0'..=b'9').contains(&slug.as_bytes()[0]) {
                     if !(crate_name.starts_with("solution-")
                         && &crate_name.as_bytes()["solution-".len()..] == slug.as_bytes())
                     {
-                        eprintln!(
-                            "{:?} has obsolete crate name: should be \"solution-{}\"",
-                            d, slug
-                        );
+                        stdout.write(d.as_bytes())?;
+                        stdout.write(b" has obsolete crate name: should be \"solution-")?;
+                        stdout.write(slug.as_bytes())?;
+                        stdout.write(b"\"\n")?;
+                        stdout.flush()?;
                     }
                 } else {
-                    eprintln!("{:?} has obsolete crate name: should be \"{}\"", d, slug);
+                    stdout.write(d.as_bytes())?;
+                    stdout.write(b" has obsolete crate name: should be \"")?;
+                    stdout.write(slug.as_bytes())?;
+                    stdout.write(b"\"\n")?;
+                    stdout.flush()?;
                 }
             }
         }
@@ -66,23 +102,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-fn get_id(path: &str) -> std::io::Result<[u8; 24]> {
-    let mut f = File::open(path)?;
-    let mut buf =
-        [0; "//! <https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/rust>".len()];
-    f.read(&mut buf)?;
-    assert!(
-        buf.starts_with(b"//! <https://www.codewars.com/kata/") && buf.ends_with(b"/train/rust>"),
-        "{} has invalid url",
-        path
-    );
-
-    let id = &buf["//! <https://www.codewars.com/kata/".len()
-        .."//! <https://www.codewars.com/kata/53da3dbb4a5168369a0000fe".len()];
-    Ok(id.try_into().unwrap())
-}
-
-fn get_crate_name(path: &str) -> std::io::Result<Box<str>> {
+fn get_crate_name(path: &str) -> std::io::Result<([u8; 128], usize, usize)> {
     let mut f = File::open(path)?;
     let mut buf = [0; 128];
     f.read(&mut buf)?;
@@ -90,12 +110,10 @@ fn get_crate_name(path: &str) -> std::io::Result<Box<str>> {
     let name_pos = "[package]\nname = \"".len();
     let name_end = name_pos + buf[name_pos..].iter().position(|&b| b == b'"').unwrap();
 
-    Ok(Box::from(unsafe {
-        std::str::from_utf8_unchecked(&buf[name_pos..name_end])
-    }))
+    Ok((buf, name_pos, name_end))
 }
 
-fn get_kata(id: [u8; 24]) -> attohttpc::Result<Box<str>> {
+fn get_kata(id: [u8; 24]) -> attohttpc::Result<([u8; 1024], usize)> {
     let mut url =
         [0; "https://www.codewars.com/api/v1/code-challenges/5917f22dd2563a36a200009c".len()];
     url[.."https://www.codewars.com/api/v1/code-challenges/".len()]
@@ -104,17 +122,17 @@ fn get_kata(id: [u8; 24]) -> attohttpc::Result<Box<str>> {
     let url = unsafe { std::str::from_utf8_unchecked(&url) };
     let mut buf = [0; 1024];
     let mut response = attohttpc::get(url).send()?;
-    let written1 = response.read(&mut buf)?;
-    let written2 = response.read(&mut buf[written1..])?;
-    let written3 = response.read(&mut buf[written1 + written2..])?;
-    Ok(Box::from(unsafe {
-        std::str::from_utf8_unchecked(buf.get_unchecked(..written1 + written2 + written3))
-    }))
+    let mut written = response.read(&mut buf)?;
+    if written < buf.len() {
+        written += response.read(&mut buf[written..])?;
+    }
+
+    Ok((buf, written))
 }
 
 fn get_kyu(kata: &str) -> u8 {
     let id_pos = kata.find("rank\":{\"id\"").unwrap() + "rank\":{\"id\":-".len();
-    kata.as_bytes()[id_pos] - b'0'
+    kata.as_bytes()[id_pos]
 }
 
 fn get_slug(kata: &str) -> &str {
