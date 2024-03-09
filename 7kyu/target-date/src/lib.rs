8494906429a5e91331042065a8b24439e4751f40
lincot <lincot@disroot.org>
2024-03-09 19:52:35 +0300

    chrono: use `try_days` instead of `days`

--- 7kyu/target-date/src/lib.rs
+++ 7kyu/target-date/src/lib.rs
@@ -1,10 +1,11 @@
 //! <https://www.codewars.com/kata/569218bc919ccba77000000b/train/rust>
 
-use chrono::{naive::NaiveDate, Duration};
+use chrono::{naive::NaiveDate, TimeDelta};
 
 pub fn date_nb_days(a0: i32, a: i32, p: i32) -> String {
     let p = (p as f64).mul_add(1. / 100. / 360., 1.);
     let days = (a as f64 / a0 as f64).log(p) as _;
-    (unsafe { NaiveDate::from_ymd_opt(2016, 1, 2).unwrap_unchecked() } + Duration::days(days))
-        .to_string()
+    (unsafe { NaiveDate::from_ymd_opt(2016, 1, 2).unwrap_unchecked() }
+        + TimeDelta::try_days(days).unwrap())
+    .to_string()
 }
