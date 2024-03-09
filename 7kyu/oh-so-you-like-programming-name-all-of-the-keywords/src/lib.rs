09f2f8fa7cf43434cf55b2488ab6db24491ded79
lincot <lincot@disroot.org>
2024-03-09 19:50:52 +0300

    `Oh, so you like programming? Name all of the keywords!`: remove "union"

--- 7kyu/oh-so-you-like-programming-name-all-of-the-keywords/src/lib.rs
+++ 7kyu/oh-so-you-like-programming-name-all-of-the-keywords/src/lib.rs
@@ -7,7 +7,7 @@ pub fn keywords() -> HashSet<&'static str> {
         "as", "async", "await", "break", "const", "continue", "crate", "dyn", "else", "enum",
         "extern", "false", "fn", "for", "if", "impl", "in", "let", "loop", "match", "mod", "move",
         "mut", "pub", "ref", "return", "self", "Self", "static", "struct", "super", "trait",
-        "true", "type", "union", "unsafe", "use", "where", "while",
+        "true", "type", "unsafe", "use", "where", "while",
     ]
     .into()
 }
