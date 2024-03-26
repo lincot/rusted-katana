325ecb5bd4374120950c5718553a8b048055dcd7
lincot <lincot@disroot.org>
2024-03-26 13:30:39 +0300

    check-katas: use tokio and handle network errors

--- check-katas/src/main.rs
+++ check-katas/src/main.rs
@@ -1,24 +1,47 @@
-use rayon::prelude::*;
+use core::{
+    mem::MaybeUninit,
+    ptr,
+    sync::atomic::{AtomicBool, Ordering},
+    time::Duration,
+};
 use std::{
     fs::{read_dir, File},
     io::{self, Read, Write},
 };
+use tokio::task::JoinSet;
 use unchecked::PushStrUnchecked;
 
-fn main() {
-    let reqwest_client = reqwest::blocking::Client::new();
-    (b'1'..=b'8').into_par_iter().for_each(|kyu| {
-        read_dir(unsafe { core::str::from_utf8_unchecked(&[kyu, b'k', b'y', b'u']) })
-            .unwrap()
-            .par_bridge()
-            .for_each(|kata_dir| {
+static mut REQWEST_CLIENT: MaybeUninit<reqwest::Client> = MaybeUninit::uninit();
+static mut BLOCKED: AtomicBool = AtomicBool::new(false);
+
+#[tokio::main]
+async fn main() {
+    unsafe { ptr::write(REQWEST_CLIENT.as_mut_ptr(), reqwest::Client::new()) };
+
+    let mut tasks = JoinSet::new();
+
+    for kyu in b'1'..=b'8' {
+        for kata_dir in
+            read_dir(unsafe { core::str::from_utf8_unchecked(&[kyu, b'k', b'y', b'u']) }).unwrap()
+        {
+            tasks.spawn(async move {
                 let kata_dir = kata_dir.unwrap().path().into_os_string();
-                check_kata(&reqwest_client, kyu, kata_dir.to_str().unwrap());
+                check_kata(
+                    unsafe { REQWEST_CLIENT.assume_init_ref() },
+                    kyu,
+                    kata_dir.to_str().unwrap(),
+                )
+                .await;
             });
-    });
+        }
+    }
+
+    while let Some(x) = tasks.join_next().await {
+        x.unwrap();
+    }
 }
 
-fn check_kata(reqwest_client: &reqwest::blocking::Client, kyu: u8, kata_dir: &str) {
+async fn check_kata(reqwest_client: &reqwest::Client, kyu: u8, kata_dir: &str) {
     let mut path = String::with_capacity(kata_dir.len() + "/benches/bench.rs".len());
     unsafe {
         path.push_str_unchecked(kata_dir);
@@ -39,7 +62,7 @@ fn check_kata(reqwest_client: &reqwest::blocking::Client, kyu: u8, kata_dir: &st
         .."//! <https://www.codewars.com/kata/53da3dbb4a5168369a0000fe".len()];
     let id = id.try_into().unwrap();
 
-    let kata = get_kata(reqwest_client, id).unwrap();
+    let kata = get_kata(reqwest_client, id).await.unwrap();
 
     let kata_kyu = get_kyu(&kata);
     if kyu != kata_kyu {
@@ -107,14 +130,43 @@ fn get_crate_name(path: &str) -> io::Result<([u8; 128], usize, usize)> {
     Ok((buf, name_pos, name_end))
 }
 
-fn get_kata(reqwest_client: &reqwest::blocking::Client, id: [u8; 24]) -> reqwest::Result<String> {
+async fn get_kata(reqwest_client: &reqwest::Client, id: [u8; 24]) -> reqwest::Result<String> {
     let mut url =
         [0; "https://www.codewars.com/api/v1/code-challenges/5917f22dd2563a36a200009c".len()];
     url[.."https://www.codewars.com/api/v1/code-challenges/".len()]
         .copy_from_slice(b"https://www.codewars.com/api/v1/code-challenges/");
     url["https://www.codewars.com/api/v1/code-challenges/".len()..].copy_from_slice(&id);
     let url = unsafe { core::str::from_utf8_unchecked(&url) };
-    reqwest_client.get(url).send()?.text()
+
+    let mut blocked_by_this = false;
+    loop {
+        while !blocked_by_this && unsafe { BLOCKED.load(Ordering::Acquire) } {
+            tokio::time::sleep(Duration::from_secs(5)).await;
+        }
+
+        let response = reqwest_client.get(url).send().await;
+        match response {
+            Ok(response) => {
+                if response.status().is_success() {
+                    if blocked_by_this {
+                        unsafe { BLOCKED.store(false, Ordering::Release) };
+                    }
+                    return response.text().await;
+                } else if ![429, 502].contains(&response.status().as_u16()) {
+                    panic!("{}", response.status());
+                }
+            }
+            Err(e) => {
+                let mut stderr = io::stderr().lock();
+                stderr.write_all(e.to_string().as_bytes()).unwrap();
+                stderr.write(b"\n").unwrap();
+            }
+        }
+        if !blocked_by_this {
+            blocked_by_this = !unsafe { BLOCKED.swap(true, Ordering::AcqRel) };
+        }
+        tokio::time::sleep(Duration::from_secs(5)).await;
+    }
 }
 
 fn get_kyu(kata: &str) -> u8 {
