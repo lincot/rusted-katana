cad31250f82049766eb63b77229191da2935dfb6
lincot <lincot@disroot.org>
2024-07-06 23:00:57 +0300

    decrease more bench sizes when run with miri

--- 6kyu/simple-fun-number-23-square-digits-sequence/benches/bench.rs
+++ 6kyu/simple-fun-number-23-square-digits-sequence/benches/bench.rs
@@ -7,7 +7,7 @@ use test::{black_box, Bencher};
 #[bench]
 fn bench(bencher: &mut Bencher) {
     bencher.iter(|| {
-        for a0 in 1..=if cfg!(miri) { 65 } else { 650 } {
+        for a0 in 1..=if cfg!(miri) { 20 } else { 650 } {
             black_box(square_digits_sequence(black_box(a0)));
         }
     });
--- 7kyu/sort-array-by-string-length/benches/bench.rs
+++ 7kyu/sort-array-by-string-length/benches/bench.rs
@@ -10,7 +10,7 @@ use test::{black_box, Bencher};
 #[bench]
 fn bench(bencher: &mut Bencher) {
     let mut rng = Pcg64Mcg::new(0xcafe_f00d_d15e_a5e5);
-    let arr: [_; 300] = array::from_fn(|_| {
+    let arr: [_; if cfg!(miri) { 20 } else { 300 }] = array::from_fn(|_| {
         let len = rng.gen_range(0..if cfg!(miri) { 200 } else { 1000 });
         let mut s = String::with_capacity(len);
         unsafe { s.as_mut_vec().set_len(len) };
