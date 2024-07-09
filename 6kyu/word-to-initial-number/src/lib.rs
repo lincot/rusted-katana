5068b947d875fc1b3164f0253de07f0430270403
lincot <lincot@disroot.org>
2024-07-09 21:39:54 +0300

    speed up "Word to initial number"

--- 6kyu/word-to-initial-number/src/lib.rs
+++ 6kyu/word-to-initial-number/src/lib.rs
@@ -1,22 +1,19 @@
 //! <https://www.codewars.com/kata/5bb148b840196d1be50000b1/train/rust>
 
-#![allow(internal_features)]
-#![feature(unicode_internals)]
-
-use core::unicode::conversions::to_lower;
+use char_to_lower::to_lower;
 
 pub fn convert(word: &str) -> u64 {
     let mut res = 0;
-    let mut map = heapless::FnvIndexMap::<_, _, 16>::new();
+    let mut map = heapless::Vec::<_, 10>::new();
     for c in word.chars().map(to_lower) {
         let len = map.len() as u8;
-        let digit = match map.entry(c) {
-            heapless::Entry::Occupied(e) => *e.get(),
-            heapless::Entry::Vacant(e) => {
-                e.insert(len).unwrap();
-                len
-            }
+        let digit = if let Some(&(_, count)) = map.iter().find(|&&(x, _)| x == c) {
+            count
+        } else {
+            map.push((c, len)).unwrap();
+            len
         };
+
         let digit = if digit == 0 {
             1
         } else if digit == 1 {
