20c1e19cc1356f05f22ceb4de763797f0457ce8a
lincot <lincot@disroot.org>
2024-07-12 20:13:32 +0300

    specify lints in `Cargo.toml` instead of flags

--- lint.sh
+++ lint.sh
@@ -10,38 +10,7 @@ sed -i "s/\(are used \)[0-9]\+\( times\)/\1$bytes_count\2/" README.md
 echo check
 cargo check --all-features --all-targets --quiet --release
 echo clippy
-cargo clippy --all-features --all-targets --no-deps --quiet --release -- \
-  -D clippy::all \
-  -D clippy::pedantic \
-  -D clippy::nursery \
-  -A clippy::must-use-candidate \
-  -A clippy::default-trait-access \
-  -A clippy::missing-panics-doc \
-  -A clippy::needless-pass-by-value \
-  -A clippy::cast-possible-truncation \
-  -A clippy::cast-sign-loss \
-  -A clippy::cast-lossless \
-  -A clippy::match-on-vec-items \
-  -A clippy::too-many-lines \
-  -A clippy::maybe-infinite-iter \
-  -A clippy::missing-errors-doc \
-  -A clippy::cast-precision-loss \
-  -A clippy::cast-possible-wrap \
-  -A clippy::implicit-hasher \
-  -A clippy::similar-names \
-  -A clippy::float-cmp \
-  -A clippy::return-self-not-must-use \
-  -A clippy::new-without-default \
-  -A clippy::many-single-char-names \
-  -A clippy::unused-io-amount \
-  -A clippy::type-complexity \
-  -A clippy::uninit-assumed-init \
-  -A clippy::zero-sized-map-values \
-  -A clippy::uninit-vec \
-  -A clippy::cognitive-complexity \
-  -A clippy::debug-assert-with-mut-call \
-  -A clippy::module-name-repetitions \
-  -A clippy::suspicious-operation-groupings
+cargo clippy --all-features --all-targets --no-deps --quiet --release
 echo update
 cargo update --quiet
 echo outdated
