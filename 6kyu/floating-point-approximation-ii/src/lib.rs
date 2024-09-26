faf1bf05b058145c6445996599e48a5b6dfddd4f
lincot <lincot@disroot.org>
2024-09-26 22:05:43 +0300

    use `f64::trunc` in `floor2`

--- 6kyu/floating-point-approximation-ii/src/lib.rs
+++ 6kyu/floating-point-approximation-ii/src/lib.rs
@@ -5,11 +5,10 @@ pub fn interp(f: fn(f64) -> f64, l: f64, u: f64, n: i32) -> Vec<f64> {
     (0..n).map(|i| floor2(f(d.mul_add(i as _, l)))).collect()
 }
 
-#[inline]
 fn floor2(mut x: f64) -> f64 {
     x *= 100.;
     if x < 0. {
-        x -= 1. - 1e-9;
+        x -= 1.;
     }
-    x as i64 as f64 / 100.
+    x.trunc() / 100.
 }
