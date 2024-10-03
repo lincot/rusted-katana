07f10836311843d55a53772039a258344eea8fd0
lincot <lincot@disroot.org>
2024-10-03 13:41:17 +0300

    small clippy changes

--- 7kyu/fast-cooking-pancakes/src/lib.rs
+++ 7kyu/fast-cooking-pancakes/src/lib.rs
@@ -1,5 +1,5 @@
 //! <https://www.codewars.com/kata/58552bdb68b034a1a80001fb/train/rust>
 
 pub fn cook_pancakes(n: u32, m: u32) -> u32 {
-    ((2 * n + m - 1) / m).max(2)
+    (2 * n).div_ceil(m).max(2)
 }
